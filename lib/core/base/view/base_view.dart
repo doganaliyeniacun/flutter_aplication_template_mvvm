617509e43f5db50eb8bb163cc46ab42da4605fa6
alidogan0716 <alidogan0716@gmail.com>
2022-03-13 03:36:46 +0300

    base view with mobx

--- lib/core/base/view/base_view.dart
+++ lib/core/base/view/base_view.dart
@@ -1,28 +1,33 @@
 import 'package:flutter/material.dart';
+import 'package:mobx/mobx.dart';
 
-class BaseView<T> extends StatefulWidget {
-  final Widget Function(BuildContext buildContext, T value) onPageBuilder;
+
+class BaseView<T extends Store> extends StatefulWidget {
+  final Widget Function(BuildContext buildContext, T viewModel) onPageBuilder;
   final T viewModel;
-  final Function(T model)? onModelReady;
+  final Function(T viewModel) onModelReady;
   final VoidCallback? onDispose;
 
   const BaseView({
     Key? key,
     required this.onPageBuilder,
     required this.viewModel,
-    this.onModelReady,
+    required this.onModelReady,
     this.onDispose,
   }) : super(key: key);
 
   @override
-  State<BaseView> createState() => _BaseViewState();
+  State<BaseView> createState() => _BaseViewState<T>();
 }
 
-class _BaseViewState extends State<BaseView> {
+class _BaseViewState<T extends Store> extends State<BaseView<T>> {
+  late T model;
+
   @override
   void initState() {
+    model = widget.viewModel;
+    widget.onModelReady(model);
     super.initState();
-    if (widget.onModelReady != null) widget.onModelReady!(widget.viewModel);
   }
 
   @override
